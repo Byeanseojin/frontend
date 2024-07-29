bc6fcc53d6a2ce576cefde3d6b58a931f42126c4
Byeanseojin <sjb12365@gmail.com>
2024-07-29 09:47:29 +0900

    api

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,14 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8081",
         changeOrigin: true,
         pathRewrite: { '^/api': '' },
+      },
+      "/aiapi": {
+        target: "http://127.0.0.1:9000/",
+        changeOrigin: true,
+        pathRewrite: { '^/aiapi': '' },
       }
     }
   }
